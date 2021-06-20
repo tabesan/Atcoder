9b3360135b5c975481abad473669f8c308e50b9d
tabeesan <tabeesan@gmail.com>
2021-06-20 17:17:08 +0900

    AC

---
+++ ABC206/ABC206-C.cpp
@@ -0,0 +1,53 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+struct UnionFind {
+    vector<int> d;
+    UnionFind(int n=0) : d(n,-1){}
+    int Find (int x){//根の番号を探す
+        if(d[x] < 0) return x;
+        return d[x] = Find(d[x]);
+    }
+    bool unite (int x, int y){
+        x = Find(x); y = Find(y);
+        if(x == y ) return false;
+        if(d[x] < d[y]){
+            swap(x,y);
+        }
+        d[x] += d[y];
+        d[y] = x;
+        return true;
+    } 
+    bool same(int x, int y){
+        return Find(x) == Find(y);
+    }
+    int size(int x) {
+        return (-d[Find(x)]);
+    }
+};
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i = 0;i < n;i++){
+        cin >> a[i];
+    }
+    int N = 200000;
+    UnionFind uf(N);
+    for(int i = 0;i < n / 2;i++){
+        uf.unite(a[i], a[n - i - 1]);
+    }
+
+    set<int> st;
+    for(int i = 0;i < n;i++){
+        st.insert(uf.Find(a[i]));
+    }
+
+    int ans = 0;
+    for(auto s : st){
+        ans += uf.size(s) - 1;
+    }
+
+    cout << ans << endl;
+}
